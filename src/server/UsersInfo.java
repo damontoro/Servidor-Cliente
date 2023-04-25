3ac6279b49fd6ba6e92dc2f56cbc5c755cdf4579
ikerc <ikerc@Iker_pc.home>
2023-04-25 13:42:38 +0200

    Cambiado el usersInfo y añadido metodos synchronized y eliminado semaforos.

--- src/server/UsersInfo.java
+++ src/server/UsersInfo.java
@@ -17,26 +17,17 @@ import message.UsersConnectedMessage;
 public class UsersInfo {
 	private Map<String, User> users;
 	private Map<String, PriorityQueue<UserPriority>> files;
-	private Semaphore semUsers, semFiles;
 	
 	public UsersInfo() {
 		users = new HashMap<String, User>();
-		files = new HashMap<String, PriorityQueue<UserPriority>>();
-		semUsers = new Semaphore(1);
-		semFiles = new Semaphore(1);
 	}
 	
-	public boolean addUser(User u) throws InterruptedException{
-		semUsers.acquire();
-		
+	public synchronized boolean addUser(User u) throws InterruptedException{
 		if(users.containsKey(u.getId())) {
-			semUsers.release();
 			return false;
 		}
 
 		users.put(u.getId(), u);
-		
-		semFiles.acquire();
 		
 		for(String file : u.getSharedInfo()){
 			if(!files.containsKey(file)){
@@ -45,77 +36,55 @@ public class UsersInfo {
 			files.get(file).add(new UserPriority(u.getId(), 0));
 		}
 		
-		semFiles.release();
-		semUsers.release();
-		
 		return true;
 	}
 	
-	public void removeUser(User u) throws InterruptedException  {
-		semUsers.acquire();
-		
+	public synchronized void removeUser(User u) throws InterruptedException  {
 		users.remove(u.getId());
-		
-		semUsers.release();
 	}
 
 	public void sendFileList(String destination, ObjectOutputStream outStream) throws Exception{
-		semFiles.acquire();
+		List<String> fileList = getFileList();
+
+		outStream.writeObject(new RetrieveFileListMessage("server", destination, fileList));
+	}
+	
+	private synchronized List<String> getFileList() {
 		List<String> fileList = new ArrayList<String>();
 		for(String file : files.keySet()) {
 			if(!files.get(file).isEmpty())
 				fileList.add(file);
 		}
-
-		outStream.writeObject(new RetrieveFileListMessage("server", destination, fileList));
-
-		semFiles.release();
+		return fileList;
 	}
+	
 
-	public void addFile(String user, String file) throws InterruptedException {
-		semUsers.acquire();
-		
+	public synchronized void addFile(String user, String file) throws InterruptedException {
 		if(!users.containsKey(user)) {
-			semUsers.release();
 			return;
 		}
 		users.get(user).addFile(file);
-		
-		semFiles.acquire();
 		
 		if(!files.containsKey(file)){
 			files.put(file, new PriorityQueue<UserPriority>((a, b) -> a.getPriority() - b.getPriority()));
 		}
 		files.get(file).add(new UserPriority(user, 0));
-		
-		semUsers.release();
-		semFiles.release();
 	}
 
-	public void removeFile(String file, String user) throws InterruptedException {
-		semUsers.acquire();
-		
+	public synchronized void removeFile(String file, String user) throws InterruptedException {
 		if(!users.containsKey(user)) {
-			semUsers.release();
 			return;
 		}
 		users.get(user).removeFile(file);
 		
-		semUsers.release();
-		semFiles.acquire();
-		
 		files.get(file).remove(new UserPriority(user, 0));
-		
-		semFiles.release();
 	}
 	
-	public String findUserWithFile(String name) throws InterruptedException{
-		semFiles.acquire();
-		
+	public synchronized String findUserWithFile(String name) throws InterruptedException{
 		if(!files.containsKey(name)) {
-			semFiles.release();
 			return null;
 		}
+		
 		UserPriority user;
 
 		//Lo hacemos así para comprobar que el supuesto usuario que tiene el archivo está conectado,
@@ -126,33 +95,29 @@ public class UsersInfo {
 
 		//Comprobamos si el usuario devuelto está conectado (en el caso de que el ultimo de la cola no lo esté)
 		if(user == null){
-			semFiles.release();
 			return null;
 		}
 
 		files.get(name).add(new UserPriority(user.getId(), user.getPriority() + 1));
-		
-		semFiles.release();
 		
 		//Comprobamos si el usuario es correcto
 		return user.getId() ;
 	}
 	
 	public void sendUsersInfo(ObjectOutputStream outStream, String destination) throws Exception {
-		semUsers.acquire();
-		
+		Set<String> ids = getUsersInfo();
+		outStream.writeObject(new UsersConnectedMessage("server", destination, ids));
+	}
+	
+	private synchronized Set<String> getUsersInfo(){
 		Set<String> ids = new HashSet<String>();
 		for(String id : users.keySet()) {
 			ids.add(id);
 		}
-		outStream.writeObject(new UsersConnectedMessage("server", destination, ids));
-		
-		semUsers.release();
+		return ids;
 	}
 	
-	public void printUsersInfo() throws InterruptedException {
-		semUsers.acquire();
-		
+	public synchronized void printUsersInfo() throws InterruptedException {
 		if(users.size() == 0) {
 			System.out.print("No user connected\n\n");
 			return;
@@ -168,8 +133,6 @@ public class UsersInfo {
 			System.out.print("--------------------------\n");
 		}
 		System.out.print("\n");
-		
-		semUsers.release();
 	}
 
 	private class UserPriority{
