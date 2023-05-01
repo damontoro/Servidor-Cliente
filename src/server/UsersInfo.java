e0f221f04b795cfbd6267343df78eee21822b5c2
ikerc <ikerc@Iker_pc>
2023-05-01 12:41:39 +0200

    Cambiado para modelo readers-writers multiples lecturas sin innanición de escritores

--- src/server/UsersInfo.java
+++ src/server/UsersInfo.java
@@ -18,87 +18,22 @@ import message.UsersConnectedMessage;
 
 //Para implementar el monitor hemos usado la clase Lock de Java para poder usar
 //la clase Condition.
-
 public class UsersInfo {
-	private Map<String, User> users;
-	private boolean usingUsersMap;
-	private Lock usersMapLock;
-	private Condition waitForUsersMap ;
-	
-	private Map<String, PriorityQueue<UserPriority>> files;
-	private boolean usingFilesMap;
-	private Lock filesMapLock;
-	private Condition waitForFilesMap;
+	private ReadersWritersMap<String, User> users;	
+	private ReadersWritersMap<String, PriorityQueue<UserPriority>> files;
 	
 	public UsersInfo() {
-		users = new HashMap<String, User>();
-		usingUsersMap = false;
-		usersMapLock = new ReentrantLock();
-		waitForUsersMap = usersMapLock.newCondition();
+		users = new ReadersWritersMap<String, User>();
 		
-		files = new HashMap<String, PriorityQueue<UserPriority>>();
-		usingFilesMap = false;
-		filesMapLock = new ReentrantLock();
-		waitForFilesMap = filesMapLock.newCondition();
-	}
-	
-	private void tryAccesingUsersMap() throws InterruptedException{
-		usersMapLock.lock();
-		try {
-			while(usingUsersMap)
-				waitForUsersMap.await();
-			usingUsersMap = true;
-		}
-		finally {
-			usersMapLock.unlock();
-		}
-	}
-	
-	private void freeAccessUsersMap() throws InterruptedException{
-		usersMapLock.lock();
-		try {
-			usingUsersMap = false;
-			waitForUsersMap.signal();
-		}
-		finally {
-			usersMapLock.unlock();
-		}
-	}
-	
-	private void tryAccesingFilesMap() throws InterruptedException{
-		filesMapLock.lock();
-		try {
-			while(usingFilesMap)
-				waitForFilesMap.await();
-			usingFilesMap = true;
-		}
-		finally {
-			filesMapLock.unlock();
-		}
-	}
-	
-	private void freeAccessFilesMap() throws InterruptedException{
-		filesMapLock.lock();
-		try {
-			usingFilesMap = false;
-			waitForFilesMap.signal();
-		}
-		finally {
-			filesMapLock.unlock();
-		}
+		files = new ReadersWritersMap<String, PriorityQueue<UserPriority>>();
 	}
 	
 	public boolean addUser(User u) throws InterruptedException{
-		tryAccesingUsersMap();
-		
 		if(users.containsKey(u.getId())) {
-			freeAccessUsersMap();
 			return false;
 		}
 
 		users.put(u.getId(), u);
-		
-		tryAccesingFilesMap();
 		
 		for(String file : u.getSharedInfo()){
 			if(!files.containsKey(file)){
@@ -107,121 +42,78 @@ public class UsersInfo {
 			files.get(file).add(new UserPriority(u.getId(), 0));
 		}
 		
-		freeAccessFilesMap();
-		freeAccessUsersMap();
-		
 		return true;
 	}
 	
 	public void removeUser(User u) throws InterruptedException {
-		tryAccesingUsersMap();
-		
 		users.remove(u.getId());
-		
-		freeAccessUsersMap();
 	}
 
 	public void sendFileList(String destination, ObjectOutputStream outStream) throws Exception{
-		
 		List<String> fileList = new ArrayList<String>();
-		
-		tryAccesingFilesMap();
 		
 		for(String file : files.keySet()) {
 			if(!files.get(file).isEmpty())
 				fileList.add(file);
 		}
 		
-		freeAccessFilesMap();
-		
 		outStream.writeObject(new RetrieveFileListMessage("server", destination, fileList));
 	}
 
 	public void addFile(String user, String file) throws InterruptedException {
-		tryAccesingUsersMap();
-		
 		if(!users.containsKey(user)) {
-			freeAccessUsersMap();
 			return;
 		}
 		users.get(user).addFile(file);
-		
-		freeAccessUsersMap();
-		tryAccesingFilesMap();
 		
 		if(!files.containsKey(file)){
 			files.put(file, new PriorityQueue<UserPriority>((a, b) -> a.getPriority() - b.getPriority()));
 		}
 		files.get(file).add(new UserPriority(user, 0));
-		
-		freeAccessFilesMap();
 	}
 
 	public void removeFile(String file, String user) throws InterruptedException {
-		tryAccesingUsersMap();
-		
 		if(!users.containsKey(user)) {
-			freeAccessUsersMap();
 			return;
 		}
 		users.get(user).removeFile(file);
 		
-		freeAccessUsersMap();
-		tryAccesingFilesMap();
-		
 		files.get(file).remove(new UserPriority(user, 0));
-		
-		freeAccessFilesMap();
 	}
 	
 	public String findUserWithFile(String name) throws InterruptedException{
-		tryAccesingFilesMap();
-		
 		if(!files.containsKey(name)) {
-			freeAccessFilesMap();
 			return null;
 		}
 		
 		UserPriority user;
-
-		tryAccesingUsersMap();
 		
 		//Lo hacemos así para comprobar que el supuesto usuario que tiene el archivo está conectado,
 		//No lo quitamos de la cola en el log off para que el coste de ese metodo sea menor
 		do{
 			user = files.get(name).poll();
 		}while(user != null && !users.containsKey(user.getId()));
-		
-		freeAccessUsersMap();
 
 		//Comprobamos si el usuario devuelto está conectado (en el caso de que el ultimo de la cola no lo esté)
 		if(user == null){
-			freeAccessFilesMap();
 			return null;
 		}
 
 		files.get(name).add(new UserPriority(user.getId(), user.getPriority() + 1));
 		
-		freeAccessFilesMap();
 		//Comprobamos si el usuario es correcto
 		return user.getId() ;
 	}
 	
-	public void sendUsersInfo(ObjectOutputStream outStream, String destination) throws Exception {
-		tryAccesingUsersMap();
-		
+	public void sendUsersInfo(ObjectOutputStream outStream, String destination) throws Exception {		
 		Set<String> ids = new HashSet<String>();
 		for(String id : users.keySet()) {
 			ids.add(id);
 		}
 		outStream.writeObject(new UsersConnectedMessage("server", destination, ids));
-		
-		freeAccessUsersMap();
 	}
 	
-	public void printUsersInfo() throws InterruptedException {
-		tryAccesingUsersMap();
-		
+	public void printUsersInfo() throws InterruptedException {		
 		if(users.size() == 0) {
 			System.out.print("No user connected\n\n");
 			return;
@@ -237,8 +129,6 @@ public class UsersInfo {
 			System.out.print("--------------------------\n");
 		}
 		System.out.print("\n");
-		
-		freeAccessUsersMap();
 	}
 
 	private class UserPriority{
@@ -265,5 +155,178 @@ public class UsersInfo {
 			}
 			return false;
 		}
+	}
+	
+	private class ReadersWritersMap<T1, T2> {
+		private Map<T1, T2> data;
+		
+		private boolean getOutOfQueue;
+		private int activeReaders;
+		private boolean writerActive;
+		private Lock lock;
+		private Condition waitingInQueue;
+		private Condition readingAllowed;
+		private Condition writingAllowed;
+		 
+		public ReadersWritersMap() {
+			data = new HashMap<T1, T2>();
+			
+			getOutOfQueue = true;
+			activeReaders = 0;
+			writerActive = false;
+			lock = new ReentrantLock();
+			waitingInQueue = lock.newCondition();
+		    readingAllowed = lock.newCondition();
+		    writingAllowed = lock.newCondition();
+		}
+		
+		private void enterQueue() throws InterruptedException {
+	        lock.lock();
+	        try {
+	            while (!getOutOfQueue) {
+	                waitingInQueue.await();
+	            }
+	            getOutOfQueue = false;
+	        } finally {
+	            lock.unlock();
+	        }
+		}
+		
+		private void notifyQueue() throws InterruptedException {
+	        lock.lock();
+	        try {
+	            getOutOfQueue = true;
+	            waitingInQueue.signal();
+	        } finally {
+	            lock.unlock();
+	        }
+		}
+		
+	    private void startReading() throws InterruptedException {
+	        lock.lock();
+	        try {
+	            while (writerActive) {
+	                readingAllowed.await();
+	            }
+	            activeReaders++;
+	        } finally {
+	            lock.unlock();
+	        }
+	    }
+
+	    private void finishReading() {
+	        lock.lock();
+	        try {
+	        	activeReaders--;
+	            if (activeReaders == 0) {
+	                writingAllowed.signal();
+	            }
+	        } finally {
+	            lock.unlock();
+	        }
+	    }
+
+	    private void startWriting() throws InterruptedException {
+	        lock.lock();
+	        try {
+	            while (writerActive || activeReaders > 0) {
+	                writingAllowed.await();
+	            }
+	            writerActive = true;
+	        } finally {
+	            lock.unlock();
+	        }
+	    }
+
+	    private void finishWriting() {
+	        lock.lock();
+	        try {
+	            writerActive = false;
+	            readingAllowed.signal();
+	            writingAllowed.signal();
+	        } finally {
+	            lock.unlock();
+	        }
+	    }
+	    
+	    public T2 get(T1 key) throws InterruptedException {
+	    	enterQueue();
+	    	
+	    	startReading();
+	    	
+	    	notifyQueue();
+	    	
+	    	T2 value = data.get(key);
+	    	
+	    	finishReading();
+	    	
+	    	return value;
+	    }
+	    
+	    public int size() throws InterruptedException {
+	    	enterQueue();
+	    	
+	    	startReading();
+	    	
+	    	notifyQueue();
+	    	
+	    	int size = data.size();
+	    	
+	    	finishReading();
+	    	
+	    	return size;
+	    }
+	    
+	    public boolean containsKey (T1 key) throws InterruptedException{
+	    	enterQueue();
+	    	
+	    	startReading();
+	    	
+	    	notifyQueue();
+	    	
+	    	boolean contained = data.containsKey(key);
+	    	
+	    	finishReading();
+	    	
+	    	return contained;
+	    }
+	    
+	    public Set<T1> keySet() throws InterruptedException {
+	    	enterQueue();
+	    	
+	    	startReading();
+	    	
+	    	notifyQueue();
+	    	
+	    	Set<T1> keySet = data.keySet();
+	    	
+	    	finishReading();
+	    	
+	    	return keySet;
+	    }
+	    
+	    public void put(T1 key, T2 value) throws InterruptedException {
+	    	enterQueue();
+	    	
+	    	startWriting();
+	    	
+	    	notifyQueue();
+	    	
+	    	data.put(key, value);
+	    	
+	    	finishWriting();
+	    }
+	    
+	    public void remove(T1 key) throws InterruptedException {
+	    	enterQueue();
+	    	
+	    	startWriting();
+	    	
+	    	notifyQueue();
+	    	
+	    	data.remove(key);
+	    	
+	    	finishWriting();
+	    }
 	}
 }
